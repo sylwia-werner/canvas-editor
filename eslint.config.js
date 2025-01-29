e4bc43e7c216a4a4c1851a86771eb19615adb236
Sylek <sylwia.werner@oke.pl>
2025-01-29 22:27:06 +0100

    add tailwind with class sorting

--- eslint.config.js
+++ eslint.config.js
@@ -6,27 +6,27 @@ import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint
-  .config(
-    { ignores: ['dist'] },
-    {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
-      files: ['**/*.{ts,tsx,js,jsx}'],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-      },
-      plugins: {
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-      },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        'react-refresh/only-export-components': [
-          'warn',
-          { allowConstantExport: true },
-        ],
-        '@typescript-eslint/no-unused-vars': 'warn',
-      },
-    },
-  )
-  .concat(eslintPluginPrettier);
+	.config(
+		{ ignores: ['dist'] },
+		{
+			extends: [js.configs.recommended, ...tseslint.configs.recommended],
+			files: ['**/*.{ts,tsx,js,jsx}'],
+			languageOptions: {
+				ecmaVersion: 2020,
+				globals: globals.browser,
+			},
+			plugins: {
+				'react-hooks': reactHooks,
+				'react-refresh': reactRefresh,
+			},
+			rules: {
+				...reactHooks.configs.recommended.rules,
+				'react-refresh/only-export-components': [
+					'warn',
+					{ allowConstantExport: true },
+				],
+				'@typescript-eslint/no-unused-vars': 'warn',
+			},
+		},
+	)
+	.concat(eslintPluginPrettier);
