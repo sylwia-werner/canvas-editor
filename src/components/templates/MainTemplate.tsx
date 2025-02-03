187e8882b980c32e9bb76a5b4ea0b363da517e7a
Sylek <sylwia.werner@oke.pl>
2025-02-03 14:15:15 +0100

    add simple editor view

--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -2,8 +2,10 @@ import { PropsWithChildren } from 'react';
 
 export const MainTemplate = ({ children }: PropsWithChildren) => {
 	return (
-		<main className="flex flex-auto justify-center px-4">
-			<div>{children}</div>
+		<main className="2-xl:px-42 m-auto flex min-h-screen max-w-screen-3xl items-center justify-center px-6 py-16 xl:px-42">
+			<div className="flex w-full flex-col gap-6 md:flex-row lg:max-h-[calc(100vh-8.25rem)]">
+				{children}
+			</div>
 		</main>
 	);
 };
--- src/pages/CanvasEditor.tsx
+++ src/pages/CanvasEditor.tsx
@@ -1,9 +1,28 @@
 import { MainTemplate } from '@/components/templates/MainTemplate';
+import welcomeImage from '@/assets/canvas-default-img.png';
 
 export const CanvasEditor = () => {
+	// TODO: Hook for canvas context
+	const image = false;
+
+	const isImageSelected = !!image;
 	return (
 		<MainTemplate>
-			<div>Hello</div>
+			<div className="flex aspect-[4/5]">
+				{isImageSelected ? (
+					<div>Canvas</div>
+				) : (
+					<img
+						src={welcomeImage}
+						alt="Create your own Poster! It's so simple. Start creating your own poster by clicking one of the action buttons located on the right"
+						className="h-full object-contain"
+					/>
+				)}
+			</div>
+			<div className="flex aspect-[4/5] flex-grow bg-gray-300">
+				Editor
+			</div>
 		</MainTemplate>
 	);
 };
+// {
