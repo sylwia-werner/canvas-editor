c8575aaacea3296785c4e43c0057431874bf8222
Sylek <sylwia.werner@oke.pl>
2025-02-03 17:32:16 +0100

    add flexible layout for different devices

--- src/components/templates/MainTemplate.tsx
+++ src/components/templates/MainTemplate.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react';
 
 export const MainTemplate = ({ children }: PropsWithChildren) => {
 	return (
-		<main className="2-xl:px-42 m-auto flex min-h-screen max-w-screen-3xl items-center justify-center px-6 py-16 xl:px-42">
-			<div className="flex w-full flex-col gap-6 md:flex-row lg:max-h-[calc(100vh-8.25rem)]">
+		<main className="2-xl:px-42 m-auto flex min-h-screen px-6 py-16 xl:px-40">
+			<div className="flex w-full flex-col items-center gap-6 lg:flex-row lg:justify-center">
 				{children}
 			</div>
 		</main>
--- src/pages/CanvasEditor.tsx
+++ src/pages/CanvasEditor.tsx
@@ -3,23 +3,23 @@ import welcomeImage from '@/assets/canvas-default-img.png';
 
 export const CanvasEditor = () => {
 	// TODO: Hook for canvas context
-	const image = false;
+	const image = 'image';
 
 	const isImageSelected = !!image;
 	return (
 		<MainTemplate>
-			<div className="flex aspect-[4/5]">
+			<div className="flex aspect-[4/5] w-full max-w-[759px]">
 				{isImageSelected ? (
-					<div>Canvas</div>
-				) : (
 					<img
 						src={welcomeImage}
 						alt="Create your own Poster! It's so simple. Start creating your own poster by clicking one of the action buttons located on the right"
-						className="h-full object-contain"
+						className="h-full w-full"
 					/>
+				) : (
+					<div>Canvas</div>
 				)}
 			</div>
-			<div className="flex aspect-[4/5] flex-grow bg-gray-300">
+			<div className="flex aspect-[4/5] w-full max-w-[759px] bg-gray-300">
 				Editor
 			</div>
 		</MainTemplate>
